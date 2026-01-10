e527a3625dbf3e08627412313c871c46b3de03bc
ramen_sys <srameen888@gmail.com>
2026-01-10 20:52:05 +0500

    added challenge 4

---
+++ challenge4.py
@@ -0,0 +1,16 @@
+def count_payment_combinations(coins,total_sum):
+
+    target = total_sum
+    if target==0:
+        return 1
+    if target<0:
+        return 0
+    if not coins:
+        return 0
+
+    else:
+        include=count_payment_combinations(coins,target-(coins[0]))
+        exclude=count_payment_combinations(coins[1:],target)
+        return include +exclude
+
+print(count_payment_combinations([2,5,3,6],10))
