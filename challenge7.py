a60fb04e2f7ccbdc16a8cd9a4a2a9b8692faf318
ramen_sys <srameen888@gmail.com>
2026-01-12 00:02:02 +0500

    implemented binary search

--- challenge7.py
+++ challenge7.py
@@ -1,12 +1,26 @@
-def koko(pile,k):
-    max_speed=max(pile)
+import math
 
-    for banana_per_hour in range(1,max_speed+1):
-        total_speed=0
+def koko(pile, k):
+    left = 1
+    right = max(pile)
+    answer = right
+
+    while left <= right:
+        mid = (left + right) // 2
+        total_hours = 0
+
         for banana in pile:
-            total_speed+= banana/banana_per_hour
-        if total_speed<k:
-           return banana_per_hour
+            total_hours += math.ceil(banana / mid)
+
+        if total_hours <= k:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1      
+
+    return answer
+
+
 
 
 print(koko([5,10,15,20],7))
